b6ae891f68fe734b97b548af9cb8ff43c4ecc970
PigneInTesta <giovannitassotti165@gmail.com>
2026-05-21 12:34:41 +0200

    TO DO: manully look for can messages for id, iq and fault_code

--- comm/comm_can.h
+++ comm/comm_can.h
@@ -96,5 +96,6 @@ void comm_can_send_status3(uint8_t id, bool replace);
 void comm_can_send_status4(uint8_t id, bool replace);
 void comm_can_send_status5(uint8_t id, bool replace);
 void comm_can_send_status6(uint8_t id, bool replace);
+void comm_can_send_status7(uint8_t id, bool replace);
 
 #endif /* COMM_CAN_H_ */
